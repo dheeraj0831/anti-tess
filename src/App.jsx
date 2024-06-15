16a71aae482fc624410ce056679ab7e957f3872d
dheeraj0831 <dheeraj.reddy100@gmail.com>
2024-06-15 11:44:33 +0530

    Specific component done

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import Landing from './pages/Landing'
 import Faculty from './pages/Faculty'
 import Upload from './pages/Upload'
 import Approve from './pages/Approve'
+import Specific from './components/Specific'
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Landing />} />
+          {/* <Route path="/" element={<Landing />} /> */}
+          <Route path="/" element={<Specific name='Sanjay' rollno='21BD1A0503' section='FS-ELITE' imageUrl='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTs8Wrp4Gpqx5pmsG-N-qma1-aLBfonzMpI_g&s' description='deepa gandu'/>} />
           <Route path="/faculty" element={<Faculty />} />
           <Route path="/upload" element={<Upload />} />
           <Route path='/approve' element={<Approve />} />
---
+++ src/components/Specific.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import { Button } from "@/components/ui/button"
+import { Badge } from "@/components/ui/badge"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+export default function Specific({ name, rollno, section, imageUrl, description }) {
+    return (
+        <>
+            <Dialog>
+                <DialogTrigger asChild>
+                    <Button>Open</Button>
+                </DialogTrigger>
+                <DialogContent className='max-w-7xl'>
+                    <Card className='m-2'>
+                        <CardHeader>
+                            <CardTitle>{name}</CardTitle>
+                            <CardDescription className="flex justiy-evenly gap-5">
+                                    {rollno}
+                                    <Badge variant="secondary">{section}</Badge>
+                            </CardDescription>
+                        </CardHeader>
+                        <CardContent className='flex flex-col items-center gap-5'>
+                            <img src={imageUrl} alt={rollno}/>
+                            {description}
+                        </CardContent>
+                        <CardFooter className='flex justify-evenly'>
+                            <Button className='w-1/3 bg-slate-700'>Approve</Button>
+                            <Button className='w-1/3'>Reject</Button>
+                        </CardFooter>
+                    </Card>
+                </DialogContent>
+            </Dialog>
+
+        </>
+    )
+}
