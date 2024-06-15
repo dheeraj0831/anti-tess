ed06307b514d1dfee8cd15426695ee99f72f422c
Sanjay-2004 <sanjay.agamamidi@gmail.com>
2024-06-15 14:55:40 +0530

    server creation

---
+++ server/index.js
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+import express from "express";
+import cors from "cors";
+import Problem from "./model/problem.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.post("/api/problems", async (req, res) => {
+  const { subject, unitTest, description, imageUrl, rollno, section, status } =
+    req.body;
+  const problem = new Problem({
+    subject,
+    unitTest,
+    description,
+    imageUrl,
+    rollno,
+    section,
+    status,
+  });
+  await problem.save();
+  res.json(problem);
+});
+
+app.put("/api/approve", async (req, res) => {
+  const { imageUrl, approval } = req.body;
+  try {
+    await Problem.findOneAndUpdate({ imageUrl }, { status: approval });
+    res.status(200).json({ message: "Status updated successfully" });
+  } catch (err) {
+    res.status(400).json({ message: "Error in updating the status" });
+  }
+});
+
+app.get("/api/approve-problems", async (req, res) => {
+  const problems = await Problem.find({ status: "default" });
+  res.json(problems);
+  // res.json({ message: "Approve Problems" });
+});
+
+app.get("/api/issues", async (req, res) => {
+  const problems = await Problem.find({ status: "approved" });
+  res.json(problems);
+  // res.json({ message: "Issues" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ server/model/problem.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const problemSchema = new mongoose.Schema({
+  subject: {
+    type: String,
+    required: true,
+  },
+  unitTest: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  rollno: {
+    type: String,
+    required: true,
+  },
+  section: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: ["approved", "rejected", "default"],
+    default: "default",
+  },
+});
+
+const Problem = mongoose.model("Problem", problemSchema);
+
+export default Problem;
