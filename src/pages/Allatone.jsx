5be994cb72537347c557dfd61b9e4bf5c0cda5a3
Sanjay-2004 <sanjay.agamamidi@gmail.com>
2024-06-27 10:41:37 +0530

    filters for issues

--- src/pages/Allatone.jsx
+++ src/pages/Allatone.jsx
@@ -4,6 +4,14 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Skeleton } from '@/components/ui/skeleton';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
 
 const Allatone = () => {
   const token = localStorage.getItem('token');
@@ -119,7 +127,7 @@ const Allatone = () => {
                   return (
                     <>
                       <p className='text-lg mx-12 my-3'>Number of Issues Pending: {filteredProblems.length}</p>
-                      <div className="mx-12 my-4">
+                      {/* <div className="mx-12 my-4">
                         <label className="mx-2">Unit Test:</label>
                         <select onChange={(e) => setUnitTestFilter(e.target.value)} value={unitTestFilter}>
                           <option value="">All</option>
@@ -143,6 +151,39 @@ const Allatone = () => {
                             <option key={index} value={value}>{value}</option>
                           ))}
                         </select>
+                      </div> */}
+                      <div className="flex justify-start gap-10 mx-12 my-4">
+                        <Select onValueChange={(value) => setUnitTestFilter(value)}>
+                          <SelectTrigger className="w-[15%]">
+                            <SelectValue placeholder="Unit Test" />
+                          </SelectTrigger>
+                          <SelectContent>
+                            {uniqueValues('unitTest').map((value, index) => (
+                              <SelectItem key={index} value={value}>{value}</SelectItem>
+                            ))}
+                          </SelectContent>
+                        </Select>
+                        <Select onValueChange={(value) => setSubjectFilter(value)}>
+                          <SelectTrigger className="w-[15%]">
+                            <SelectValue placeholder="Subject" />
+                          </SelectTrigger>
+                          <SelectContent>
+                            {uniqueValues('subject').map((value, index) => (
+                              <SelectItem key={index} value={value}>{value}</SelectItem>
+                            ))}
+                          </SelectContent>
+                        </Select>
+                        <Select onValueChange={(value) => setSectionFilter(value)}>
+                          <SelectTrigger className="w-[15%]">
+                            <SelectValue placeholder="Section" />
+                          </SelectTrigger>
+                          <SelectContent>
+                            {uniqueValues('section').map((value, index) => (
+                              <SelectItem key={index} value={value}>{value}</SelectItem>
+                            ))}
+                          </SelectContent>
+                        </Select>
+
                       </div>
                       <div className='grid grid-cols-4 max-sm:grid-cols-1 gap-5 mx-12 my-8'>
                         {filteredProblems.map((problem) => (
@@ -164,30 +205,38 @@ const Allatone = () => {
                   return (
                     <>
                       <p className='text-lg mx-12 my-3'>Number of Issues Approved: {filteredProblems.length}</p>
-                      <div className="mx-12 my-4">
-                        <label className="mx-2">Unit Test:</label>
-                        <select onChange={(e) => setUnitTestFilter(e.target.value)} value={unitTestFilter}>
-                          <option value="">All</option>
-                          {uniqueValues('unitTest').map((value, index) => (
-                            <option key={index} value={value}>{value}</option>
-                          ))}
-                        </select>
+                      <div className="flex justify-start gap-10 mx-12 my-4">
+                        <Select onValueChange={(value) => setUnitTestFilter(value)}>
+                          <SelectTrigger className="w-[15%]">
+                            <SelectValue placeholder="Unit Test" />
+                          </SelectTrigger>
+                          <SelectContent>
+                            {uniqueValues('unitTest').map((value, index) => (
+                              <SelectItem key={index} value={value}>{value}</SelectItem>
+                            ))}
+                          </SelectContent>
+                        </Select>
+                        <Select onValueChange={(value) => setSubjectFilter(value)}>
+                          <SelectTrigger className="w-[15%]">
+                            <SelectValue placeholder="Subject" />
+                          </SelectTrigger>
+                          <SelectContent>
+                            {uniqueValues('subject').map((value, index) => (
+                              <SelectItem key={index} value={value}>{value}</SelectItem>
+                            ))}
+                          </SelectContent>
+                        </Select>
+                        <Select onValueChange={(value) => setSectionFilter(value)}>
+                          <SelectTrigger className="w-[15%]">
+                            <SelectValue placeholder="Section" />
+                          </SelectTrigger>
+                          <SelectContent>
+                            {uniqueValues('section').map((value, index) => (
+                              <SelectItem key={index} value={value}>{value}</SelectItem>
+                            ))}
+                          </SelectContent>
+                        </Select>
 
-                        <label className="mx-2">Subject:</label>
-                        <select onChange={(e) => setSubjectFilter(e.target.value)} value={subjectFilter}>
-                          <option value="">All</option>
-                          {uniqueValues('subject').map((value, index) => (
-                            <option key={index} value={value}>{value}</option>
-                          ))}
-                        </select>
-
-                        <label className="mx-2">Section:</label>
-                        <select onChange={(e) => setSectionFilter(e.target.value)} value={sectionFilter}>
-                          <option value="">All</option>
-                          {uniqueValues('section').map((value, index) => (
-                            <option key={index} value={value}>{value}</option>
-                          ))}
-                        </select>
                       </div>
                       <div className='grid grid-cols-4 max-sm:grid-cols-1 gap-5 mx-12 my-8'>
                         {filteredProblems.map((problem) => (
@@ -209,30 +258,38 @@ const Allatone = () => {
                   return (
                     <>
                       <p className='text-lg mx-12 my-3'>Number of Issues Rejected: {filteredProblems.length}</p>
-                      <div className="mx-12 my-4">
-                        <label className="mx-2">Unit Test:</label>
-                        <select onChange={(e) => setUnitTestFilter(e.target.value)} value={unitTestFilter}>
-                          <option value="">All</option>
-                          {uniqueValues('unitTest').map((value, index) => (
-                            <option key={index} value={value}>{value}</option>
-                          ))}
-                        </select>
+                      <div className="flex justify-start gap-10 mx-12 my-4">
+                        <Select onValueChange={(value) => setUnitTestFilter(value)}>
+                          <SelectTrigger className="w-[15%]">
+                            <SelectValue placeholder="Unit Test" />
+                          </SelectTrigger>
+                          <SelectContent>
+                            {uniqueValues('unitTest').map((value, index) => (
+                              <SelectItem key={index} value={value}>{value}</SelectItem>
+                            ))}
+                          </SelectContent>
+                        </Select>
+                        <Select onValueChange={(value) => setSubjectFilter(value)}>
+                          <SelectTrigger className="w-[15%]">
+                            <SelectValue placeholder="Subject" />
+                          </SelectTrigger>
+                          <SelectContent>
+                            {uniqueValues('subject').map((value, index) => (
+                              <SelectItem key={index} value={value}>{value}</SelectItem>
+                            ))}
+                          </SelectContent>
+                        </Select>
+                        <Select onValueChange={(value) => setSectionFilter(value)}>
+                          <SelectTrigger className="w-[15%]">
+                            <SelectValue placeholder="Section" />
+                          </SelectTrigger>
+                          <SelectContent>
+                            {uniqueValues('section').map((value, index) => (
+                              <SelectItem key={index} value={value}>{value}</SelectItem>
+                            ))}
+                          </SelectContent>
+                        </Select>
 
-                        <label className="mx-2">Subject:</label>
-                        <select onChange={(e) => setSubjectFilter(e.target.value)} value={subjectFilter}>
-                          <option value="">All</option>
-                          {uniqueValues('subject').map((value, index) => (
-                            <option key={index} value={value}>{value}</option>
-                          ))}
-                        </select>
-
-                        <label className="mx-2">Section:</label>
-                        <select onChange={(e) => setSectionFilter(e.target.value)} value={sectionFilter}>
-                          <option value="">All</option>
-                          {uniqueValues('section').map((value, index) => (
-                            <option key={index} value={value}>{value}</option>
-                          ))}
-                        </select>
                       </div>
                       <div className='grid grid-cols-4 max-sm:grid-cols-1 gap-5 mx-12 my-8'>
                         {filteredProblems.map((problem) => (
@@ -251,32 +308,41 @@ const Allatone = () => {
           ) : (
             <>
               <p className='text-lg mx-12 my-3'>Number of Issues raised: {filterProblems().length}</p>
-              {/* <div className="mx-12 my-4">
-                <label className="mx-2">Unit Test:</label>
-                <select onChange={(e) => setUnitTestFilter(e.target.value)} value={unitTestFilter}>
-                  <option value="">All</option>
-                  {uniqueValues('unitTest').map((value, index) => (
-                    <option key={index} value={value}>{value}</option>
-                  ))}
-                </select>
+              <div className="flex justify-start items-center gap-10 mx-12 my-4">
+                <p className='text-md'>Filters: </p>
+                <Select onValueChange={(value) => setUnitTestFilter(value)}>
+                  <SelectTrigger className="w-[15%]">
+                    <SelectValue placeholder="Unit Test" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {uniqueValues('unitTest').map((value, index) => (
+                      <SelectItem key={index} value={value}>{value}</SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+                <Select onValueChange={(value) => setSubjectFilter(value)}>
+                  <SelectTrigger className="w-[15%]">
+                    <SelectValue placeholder="Subject" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {uniqueValues('subject').map((value, index) => (
+                      <SelectItem key={index} value={value}>{value}</SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+                <Select onValueChange={(value) => setSectionFilter(value)}>
+                  <SelectTrigger className="w-[15%]">
+                    <SelectValue placeholder="Section" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {uniqueValues('section').map((value, index) => (
+                      <SelectItem key={index} value={value}>{value}</SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
 
-                <label className="mx-2">Subject:</label>
-                <select onChange={(e) => setSubjectFilter(e.target.value)} value={subjectFilter}>
-                  <option value="">All</option>
-                  {uniqueValues('subject').map((value, index) => (
-                    <option key={index} value={value}>{value}</option>
-                  ))}
-                </select>
-
-                <label className="mx-2">Section:</label>
-                <select onChange={(e) => setSectionFilter(e.target.value)} value={sectionFilter}>
-                  <option value="">All</option>
-                  {uniqueValues('section').map((value, index) => (
-                    <option key={index} value={value}>{value}</option>
-                  ))}
-                </select>
-              </div> */}
-              <div className='grid grid-cols-4 max-sm:grid-cols-1 gap-5 mx-12 my-8'>
+              </div>
+              {filterProblems.length ? (<div className='grid grid-cols-4 max-sm:grid-cols-1 gap-5 mx-12 my-8'>
                 {filterProblems().map((problem) => (
                   <Probpreview
                     key={problem._id}
@@ -284,7 +350,10 @@ const Allatone = () => {
                     user={user}
                   />
                 ))}
-              </div>
+              </div>) : (
+                <h1 className='text-4xl text-center mt-20'>No Issues to Show</h1>
+
+              )}
             </>
           )}
         </>
